e16ab5f50a622b4a2cb2cc4430a11400dcb44868
Yanek K <yanek.dev@gmail.com>
2021-04-05 04:05:11 -0400

    Comment on Post

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,12 @@ const functions = require("firebase-functions");
 const app = require("express")();
 const FBAuth = require("./utils/fbAuth");
 
-const { getAllPosts, postOnePost } = require("./handlers/posts");
+const {
+  getAllPosts,
+  postOnePost,
+  getPost,
+  commentOnPost,
+} = require("./handlers/posts");
 const {
   signup,
   login,
@@ -14,6 +19,15 @@ const {
 // Post routes
 app.get("/posts", getAllPosts);
 app.post("/post", FBAuth, postOnePost);
+app.get("/post/:postId", getPost);
+app.post("/post/:postId/comment", FBAuth, commentOnPost);
+
+//TODO::
+// delete post
+// like a post
+// unlike a post
+// comment on a post
+//
 
 //Signup Route
 app.post("/signup", signup);
