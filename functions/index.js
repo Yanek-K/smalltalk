66f0e631beea4d4ade8d65ac10da33a1be903a5d
Yanek K <yanek.dev@gmail.com>
2021-04-05 19:32:28 -0400

    Create and Get Notifications

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,8 @@ const functions = require("firebase-functions");
 const app = require("express")();
 const FBAuth = require("./utils/fbAuth");
 
+const { db } = require("./utils/admin");
+
 const {
   getAllPosts,
   postOnePost,
@@ -18,6 +20,8 @@ const {
   uploadImage,
   addUserDetails,
   getAuthenticatedUser,
+  getUserDetails,
+  markNotificationsRead,
 } = require("./handlers/users");
 
 // Post routes
@@ -28,9 +32,6 @@ app.post("/post/:postId/comment", FBAuth, commentOnPost);
 app.get("/post/:postId/like", FBAuth, likePost);
 app.get("/post/:postId/unlike", FBAuth, unlikePost);
 app.delete("/post/:postId", FBAuth, deletePost);
-
-//TODO::
-// delete post
 
 //Signup Route
 app.post("/signup", signup);
@@ -47,4 +48,73 @@ app.post("/user", FBAuth, addUserDetails);
 // Get Own User Details
 app.get("/user", FBAuth, getAuthenticatedUser);
 
+app.get("/user/:handle", getUserDetails);
+app.post("/notifications", FBAuth, markNotificationsRead);
+
 exports.api = functions.https.onRequest(app);
+
+exports.createNotificationOnLike = functions.firestore // .region("northamerica-northeast1")
+  .document("likes/{id}")
+  .onCreate((snapshot) => {
+    db.doc(`/posts/${snapshot.data().postId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "like",
+            read: false,
+            postId: doc.id,
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.deleteNotificationOnUnlike = functions.firestore // .region("northamerica-northeast1")
+  .document("likes/{id}")
+  .onDelete((snapshot) => {
+    db.doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.createNotificationOnComment = functions.firestore // .region("northamerica-northeast1")
+  .document("comments/{id}")
+  .onCreate((snapshot) => {
+    db.doc(`/posts/${snapshot.data().postId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().userHandle,
+            sender: snapshot.data().userHandle,
+            type: "comment",
+            read: false,
+            postId: doc.id,
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
