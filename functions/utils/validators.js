887d8d91e021d8b15913a10531057c4736803ade
Yanek K <yanek.dev@gmail.com>
2021-04-06 04:42:58 -0400

    Fixed Spelling Mistake

--- functions/utils/validators.js
+++ functions/utils/validators.js
@@ -36,8 +36,8 @@ exports.validateSignupData = (data) => {
 exports.validateLoginData = (data) => {
   let errors = {};
 
-  if (isEmpty(data.email)) errors.email = "Must no be empty";
-  if (isEmpty(data.password)) errors.password = "Must no be empty";
+  if (isEmpty(data.email)) errors.email = "Must not be empty";
+  if (isEmpty(data.password)) errors.password = "Must not be empty";
 
   return {
     errors,
